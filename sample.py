903395f65f52132b61f25b3e277407a54d24f37e
Ben Mosher <me@benmosher.com>
2018-02-07 13:52:44 -0500

    CLI interface consistency (hyphens all around)

--- sample.py
+++ sample.py
@@ -13,7 +13,7 @@ from six import text_type
 def main():
     parser = argparse.ArgumentParser(
                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--save_dir', type=str, default='save',
+    parser.add_argument('--job-dir', type=str, required=True,
                         help='model directory to store checkpointed models')
     parser.add_argument('-n', type=int, default=500,
                         help='number of characters to sample')
@@ -30,15 +30,15 @@ def main():
 
 
 def sample(args):
-    with open(os.path.join(args.save_dir, 'config.pkl'), 'rb') as f:
+    with open(os.path.join(args.job_dir, 'config.pkl'), 'rb') as f:
         saved_args = cPickle.load(f)
-    with open(os.path.join(args.save_dir, 'chars_vocab.pkl'), 'rb') as f:
+    with open(os.path.join(args.job_dir, 'chars_vocab.pkl'), 'rb') as f:
         chars, vocab = cPickle.load(f)
     model = Model(saved_args, training=False)
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
         saver = tf.train.Saver(tf.global_variables())
-        ckpt = tf.train.get_checkpoint_state(args.save_dir)
+        ckpt = tf.train.get_checkpoint_state(args.job_dir)
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess, ckpt.model_checkpoint_path)
             print(model.sample(sess, chars, vocab, args.n, args.prime,
--- train.py
+++ train.py
@@ -18,33 +18,33 @@ def main():
                         help='data directory containing input.txt')
     parser.add_argument('--job-dir', type=str, required=True,
                         help='directory to store checkpointed models')
-    parser.add_argument('--log_dir', type=str, default='logs',
+    parser.add_argument('--log-dir', type=str, default='logs',
                         help='directory to store tensorboard logs')
-    parser.add_argument('--rnn_size', type=int, default=128,
+    parser.add_argument('--rnn-size', type=int, default=128,
                         help='size of RNN hidden state')
-    parser.add_argument('--num_layers', type=int, default=2,
+    parser.add_argument('--num-layers', type=int, default=2,
                         help='number of layers in the RNN')
     parser.add_argument('--model', type=str, default='lstm',
                         help='rnn, gru, lstm, or nas')
-    parser.add_argument('--batch_size', type=int, default=50,
+    parser.add_argument('--batch-size', type=int, default=50,
                         help='minibatch size')
-    parser.add_argument('--seq_length', type=int, default=50,
+    parser.add_argument('--seq-length', type=int, default=50,
                         help='RNN sequence length')
-    parser.add_argument('--num_epochs', type=int, default=50,
+    parser.add_argument('--num-epochs', type=int, default=50,
                         help='number of epochs')
-    parser.add_argument('--save_every', type=int, default=1000,
+    parser.add_argument('--save-every', type=int, default=1000,
                         help='save frequency')
     parser.add_argument('--grad_clip', type=float, default=5.,
                         help='clip gradients at this value')
-    parser.add_argument('--learning_rate', type=float, default=0.002,
+    parser.add_argument('--learning-rate', type=float, default=0.002,
                         help='learning rate')
-    parser.add_argument('--decay_rate', type=float, default=0.97,
+    parser.add_argument('--decay-rate', type=float, default=0.97,
                         help='decay rate for rmsprop')
-    parser.add_argument('--output_keep_prob', type=float, default=1.0,
+    parser.add_argument('--output-keep-prob', type=float, default=1.0,
                         help='probability of keeping weights in the hidden layer')
-    parser.add_argument('--input_keep_prob', type=float, default=1.0,
+    parser.add_argument('--input-keep-prob', type=float, default=1.0,
                         help='probability of keeping weights in the input layer')
-    parser.add_argument('--init_from', type=str, default=None,
+    parser.add_argument('--init-from', type=str, default=None,
                         help="""continue training from saved model at this path. Path must contain files saved by previous training process:
                             'config.pkl'        : configuration;
                             'chars_vocab.pkl'   : vocabulary definitions;
